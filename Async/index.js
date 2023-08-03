d9e9e50f2ec679926b762681db3187c44d7fd6c5
patryk-pi <patryk.piasecki1@gmail.com>
2023-08-03 16:33:08 +0200

    returning values from async functions

--- Async/index.js
+++ Async/index.js
@@ -33,10 +33,28 @@ const getDogPic = async () => {
     console.log('Random dog image saved to file');
   } catch (err) {
     console.log(err);
+    throw err;
   }
+
+  return '2: READY 🐶';
 };
 
-getDogPic();
+// console.log('1: Will get dog pics');
+// getDogPic().then((x) => {
+//   console.log(x);
+//   console.log('3: Done getting pics!');
+// });
+
+(async () => {
+  try {
+    console.log('1: Will get dog pics');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Done getting pics!');
+  } catch (err) {
+    console.log(err);
+  }
+})();
 
 // readFilePro(`${__dirname}/dog.txt`)
 //   .then((data) => {
